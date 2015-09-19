0846ca0e937d7fed8629520980c9742447d30b00
Jon Bloomer <jon@freaksauce.com>
2015-09-19 16:03:59 +1000

    added http package and copied across api.call method from blaze version

--- client/Home.jsx
+++ client/Home.jsx
@@ -1,10 +1,29 @@
+Meteor.subscribe("posts");
+Session.set('ppp', 10);
+
 Home = React.createClass({
 
   mixins: [ReactMeteorData],
 
   getMeteorData() {
+    Meteor.call('getPosts', Session.get('ppp'), function(error, result) {
+
+      if (error) {
+        console.log(error);
+      }else{
+        // console.log('limit :'+Session.get('ppp'));
+        console.log(result);
+        // Session.set('posts', result);
+        // Meteor.call('updatePosts', result);
+
+      }
+
+    });
+
     return {
+
     }
+
   },
 
   getInitialState: function() {
---
+++ server/server.js
@@ -0,0 +1,58 @@
+var apiCall = function (apiUrl, callback) {
+  try {
+    var response = HTTP.get(apiUrl).data;
+    callback(null, response);
+  } catch (error) {
+    if (error.response) {
+      var errorCode = error.response.data.code;
+      var errorMessage = error.response.data.message;
+    } else {
+      var errorCode = 500;
+      var errorMessage = 'Cannot access the API';
+    }
+    var myError = new Meteor.Error(errorCode, errorMessage);
+    callback(myError, null);
+  }
+}
+
+Meteor.methods({
+
+  // updatePosts: function(data) {
+  //
+  //   _.each(data, function(post) {
+  //     // console.log(post);
+  //     myID = post.ID;
+  //     myTitle = post.title;
+  //     myDate = post.date;
+  //     myContent = post.content;
+  //
+  //     Posts.upsert({ID: myID}, {
+  //       // Modifier
+  //       $set: {
+  //           ID: myID,
+  //           title: myTitle,
+  //           date: myDate,
+  //           content: myContent
+  //       }
+  //     });
+  //
+  //   });
+  //   return true;
+  // },
+
+  getPosts: function(ppp) {
+    console.log('getPosts called');
+    this.unblock();
+    var apiUrl = "http://guitarnoize.com/wp-json/posts?filter[posts_per_page]="+ppp;
+    console.log(apiUrl);
+    var response = Meteor.wrapAsync(apiCall)(apiUrl);
+    return response;
+  }
+});
+
+Meteor.publish('posts', function () {
+  return Posts.find({}, {sort: {date: -1}});
+});
+Meteor.publish('post', function(postId) {
+  return Posts.find({ID: postId});
+});
