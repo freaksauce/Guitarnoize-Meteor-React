49d95ff23ded45ea2c4999fde1a975eacd9079c9
Jon Bloomer <jon@freaksauce.com>
2015-09-23 23:06:02 +1000

    fixed publish method to parseInt

--- server/server.js
+++ server/server.js
@@ -57,5 +57,5 @@ Meteor.publish('posts', function () {
   return PostsCollection.find({}, {sort: {date: -1}});
 });
 Meteor.publish('post', function(postId) {
-  return PostsCollection.find({});
+  return PostsCollection.find({ID: parseInt(postId)});
 });
